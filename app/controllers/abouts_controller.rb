f148436606bcf834b297588484f6b9404dee11f8
Parispan <5410610215@student.tu.ac.th>
2014-02-16 08:09:08 -0800

    test

---
+++ app/controllers/abouts_controller.rb
@@ -0,0 +1,51 @@
+class AboutsController < ApplicationController
+  # GET /abouts
+  # GET /abouts.json
+  def index
+    
+  end
+
+  # GET /abouts/1
+  # GET /abouts/1.json
+  def show
+   
+  end
+
+  # GET /abouts/new
+  # GET /abouts/new.json
+  def new
+  
+  end
+
+  # GET /abouts/1/edit
+  def edit
+ 
+  end
+
+  # POST /abouts
+  # POST /abouts.json
+  def create
+   
+  end
+
+  # PATCH/PUT /abouts/1
+  # PATCH/PUT /abouts/1.json
+  def update
+   
+  end
+
+  # DELETE /abouts/1
+  # DELETE /abouts/1.json
+  def destroy
+    
+  end
+
+  private
+
+    # Use this method to whitelist the permissible parameters. Example:
+    # params.require(:person).permit(:name, :age)
+    # Also, you can specialize this method with per-user checking of permissible attributes.
+    def about_params
+     
+    end
+end
